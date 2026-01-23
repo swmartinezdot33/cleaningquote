6452b4631842dc8b1cf64c32d8999fba4ad0a982
Steven Martinez <steven@ultimateagentsystems.com>
2026-01-22 21:29:32 -0600

    fix: allow iframe embedding from cross-origin sites
    Changed X-Frame-Options from SAMEORIGIN to ALLOWALL to permit
    the widget to be embedded on third-party websites without
    browser blocking the iframe connection.

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
           },
           {
             key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
+            value: 'ALLOWALL'
           },
           {
             key: 'X-Content-Type-Options',
